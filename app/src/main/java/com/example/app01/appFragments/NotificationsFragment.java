0af9a05622e280c233a1048f35efbb2d0a0cff4f
SidP <sidp919@gmail.com>
2019-10-13 17:43:41 +0530

    made more changes in NotificationsFragment: now app sends and recieves a notification

--- app/src/main/java/com/example/app01/appFragments/NotificationsFragment.java
+++ app/src/main/java/com/example/app01/appFragments/NotificationsFragment.java
@@ -1,5 +1,8 @@
 package com.example.app01.appFragments;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +14,28 @@ import com.example.app01.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NotificationsFragment extends Fragment implements View.OnClickListener {
 
     private final static String PRIMARY_CHANNEL_ID = "primary_notification_channel";//Every
-    private Button notifyMeButton;
-    private Button updateButton;
-    private Button cancelButton;
     // notification channel must be associated with an ID that is unique within your package.
     // You use this channel ID later,
     // to post your notifications.
+
+    private static final int NOTIFICATION_ID = 0;//You need to associate the notification with a
+    // notification ID so that your code can update or cancel
+    // the notification in the future.
+
+    private Button notifyMeButton;
+    private Button updateButton;
+    private Button cancelButton;
+
+    //The Android system uses the NotificationManager class to deliver notifications to the user
+    private NotificationManager mNotifyManager;
 
     @Nullable
     @Override
@@ -41,6 +55,9 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
 
         cancelButton = view.findViewById(R.id.cancelNotificationButton);
         cancelButton.setOnClickListener(this);
+
+        // you must call createNotificationChannel(). If you miss this step, your app crashes!
+        createNotificationChannel();
     }
 
     @Override
@@ -58,7 +75,46 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
         }
     }
 
-    private void sendNotifications() {
+    //notification channels are only available in API 26 and higher,
+    // add a condition to check for the device's API version.
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager)
+                getContext().getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
 
+            // configure the notificationChannel object's initial settings
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+            //
+        }
+    }
+
+    //Notifications are created using the NotificationCompat.Builder class,
+    // which allows you to set the content and behavior of the notification.
+    // A notification can contain the following elements:-----
+    //
+    //Icon (required), which you set in your code using the setSmallIcon() method.
+    //Title (optional), which you set using setContentTitle().
+    //Detail text (optional), which you set using setContentText().
+    private NotificationCompat.Builder getNotificationBuilder() {
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(getContext(), PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.profile_icon_img_foreground);
+        return notifyBuilder;
+    }
+
+    private void sendNotifications() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 }
